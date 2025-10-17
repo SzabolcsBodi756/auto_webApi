62d708d60461c1d9a7de58cb3d6b1d61b6ad52dc
SzabolcsBodi756 <bodisz@kkszki.hu>
2025-10-17 11:54:30 +0200

    AddNewRecord is ready

--- WebApplication1/Controllers/CarsController.cs
+++ WebApplication1/Controllers/CarsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Validations;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -28,5 +29,34 @@ namespace WebApplication1.Controllers
             
         }
 
+
+        [HttpPost]
+        public ActionResult<Car> AddNewRecord(Car car)
+        {
+
+            using (var context = new CarDbContext())
+            {
+                var newCar = new Car
+                {
+
+                    Brand = car.Brand,
+                    Type = car.Type,
+                    Color = car.Color,
+                    Year = car.Year
+
+                };
+
+                if(newCar != null)
+                {
+                    context.Cars.Add(newCar);
+                    context.SaveChanges();
+                    return StatusCode(201, newCar);
+                }
+                else
+                {
+                    return BadRequest(new { message = "Sikertelen lekérdezés!" });
+                }
+            }
+        }
     }
 }
